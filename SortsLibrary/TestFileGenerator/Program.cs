63839bc8ad1fff2a4f806012bb5d875c7ea1399d
ruszrok <judanov.v@gmail.com>
2013-11-30 19:05:14 +0400

    Implemented random file generation

--- SortsLibrary/TestFileGenerator/Program.cs
+++ SortsLibrary/TestFileGenerator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,47 @@ namespace TestFileGenerator
 {
     class Program
     {
+        const string OutFileFormat = "{0} ";
+
+        static void RandomArrayGeneration(StreamWriter outFile, int arraySize, int lowBound, int highBound)
+        {
+            var generator = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < arraySize; i++)
+            {
+                var nextValue = generator.Next(lowBound, highBound);
+                outFile.Write(String.Format(OutFileFormat, nextValue));
+            }
+        }
+
         static void Main(string[] args)
         {
+            var options = new CommandLineOptions();
+            if (args.Length > 0)
+            {
+                using (var fs = new FileStream(options.FileName, FileMode.Create))
+                using (var sw = new StreamWriter(new BufferedStream(fs)))
+                {
+                    sw.WriteLine(options.ArraySize);
+                    var orderType = (ArrayOrder) options.OrderType;
+
+                    switch(orderType)
+                    {
+                        case ArrayOrder.Random:
+                            RandomArrayGeneration(sw, options.ArraySize, options.LowElementBound, options.HighElementBound);
+                            break;
+                        case ArrayOrder.Asc:
+                            break;
+                        case ArrayOrder.Desc:
+                            break;
+                    }
+
+                    sw.Flush();
+                }
+            }
+            else
+            {
+                Console.WriteLine(options.GetUsage());
+            }
         }
     }
 }
