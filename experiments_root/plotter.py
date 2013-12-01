c79f83203799d8454bf99a5a342edf7c32353056
ruszrok <judanov.v@gmail.com>
2013-12-01 10:58:08 +0400

    refactoring in plotter

--- experiments_root/plotter.py
+++ experiments_root/plotter.py
@@ -2,6 +2,20 @@ import os
 import sys
 import matplotlib.pyplot as plt
 
+def get_q_h_data(base_folder, q_time_file, h_time_file):
+	h = []
+	q = []
+	for root, dirs, files in os.walk(base_folder):
+		for d in dirs:
+			q_f_path = os.path.join(base_folder, d, q_time_file)
+			h_f_path = os.path.join(base_folder, d, h_time_file)
+			with open(q_f_path, 'r') as f:
+				q.append(float(f.readline().strip()))
+			with open(h_f_path, 'r') as f:
+				h.append(float(f.readline().strip()))
+	return q, h
+
+
 if len(sys.argv) < 2:
 	print 'Specify experiment folder. Syntax python plotter.py experiment_folder'	
 else:
@@ -15,44 +29,14 @@ else:
 
 	print 'Information gathering: ', experiment_folder
 
-	q_random_times = []
-	h_random_times = []
 	random_folders_path = os.path.join(experiment_folder, random_folder)
+	q_random_times, h_random_times = get_q_h_data(random_folders_path, q_time_file, h_time_file)
 
-	for root, dirs, files in os.walk(random_folders_path):
-		for d in dirs:
-			q_f_path = os.path.join(random_folders_path, d, q_time_file)
-			h_f_path = os.path.join(random_folders_path, d, h_time_file)
-			with open(q_f_path, 'r') as f:
-				q_random_times.append(float(f.readline().strip()))
-			with open(h_f_path, 'r') as f:
-				q_random_times.append(float(f.readline().strip()))
-
-	q_asc_times = []
-	h_asc_times = []
 	asc_folders_path = os.path.join(experiment_folder, asc_folder)
+	q_asc_times, h_asc_times = get_q_h_data(asc_folders_path, q_time_file, h_time_file)
 
-	for root, dirs, files in os.walk(asc_folders_path):
-		for d in dirs:
-			q_f_path = os.path.join(asc_folders_path, d, q_time_file)
-			h_f_path = os.path.join(asc_folders_path, d, h_time_file)
-			with open(q_f_path, 'r') as f:
-				q_asc_times.append(float(f.readline().strip()))
-			with open(h_f_path, 'r') as f:
-				h_asc_times.append(float(f.readline().strip()))
-
-	q_desc_times = []
-	h_desc_times = []
 	desc_folders_path = os.path.join(experiment_folder, asc_folder)
-
-	for root, dirs, files in os.walk(desc_folders_path):
-		for d in dirs:
-			q_f_path = os.path.join(desc_folders_path, d, q_time_file)
-			h_f_path = os.path.join(desc_folders_path, d, h_time_file)
-			with open(q_f_path, 'r') as f:
-				q_desc_times.append(float(f.readline().strip()))
-			with open(h_f_path, 'r') as f:
-				h_desc_times.append(float(f.readline().strip()))
+	q_desc_times, h_desc_times = get_q_h_data(desc_folders_path, q_time_file, h_time_file)
 
 	plt.plot(q_random_times, c='b', title='Quicksort with random array')
 	plt.plot(q_asc_times, c='b', title='Quicksort with asc array')
