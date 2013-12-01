1365af252eaaaedab8a46c5525a7dde6c5737ca1
ruszrok <judanov.v@gmail.com>
2013-12-01 15:06:25 +0400

    Added csv output for plotter

--- experiments_root/plotter.py
+++ experiments_root/plotter.py
@@ -1,12 +1,14 @@
 import os
 import sys
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+
 
 def get_q_h_data(base_folder, q_time_file, h_time_file):
 	h = []
 	q = []
 	for root, dirs, files in os.walk(base_folder):
-		for d in dirs:
+		f = 0
+		for d in sorted(dirs, key = int):
 			q_f_path = os.path.join(base_folder, d, q_time_file)
 			h_f_path = os.path.join(base_folder, d, h_time_file)
 			with open(q_f_path, 'r') as f:
@@ -14,6 +16,13 @@ def get_q_h_data(base_folder, q_time_file, h_time_file):
 			with open(h_f_path, 'r') as f:
 				h.append(float(f.readline().strip()))
 	return q, h
+
+def csv_output(csv_file_format, base_folder, q_times, h_times):
+	csv_file_name = csv_file_format % base_folder
+	with open(csv_file_name, 'w') as f:
+				f.write(','.join(map(str, q_times)) + '\n')
+				f.write(','.join(map(str, h_times)) + '\n')
+
 
 
 if len(sys.argv) < 2:
@@ -24,26 +33,32 @@ else:
 	desc_folder = 'desc'
 	q_time_file = 'test_q_t.time'
 	h_time_file = 'test_h_t.time'
-
 	experiment_folder = sys.argv[1];
+
+	csv_file_format = 'results_' + experiment_folder + '_%s.csv'
 
 	print 'Information gathering: ', experiment_folder
 
 	random_folders_path = os.path.join(experiment_folder, random_folder)
 	q_random_times, h_random_times = get_q_h_data(random_folders_path, q_time_file, h_time_file)
 
+
 	asc_folders_path = os.path.join(experiment_folder, asc_folder)
 	q_asc_times, h_asc_times = get_q_h_data(asc_folders_path, q_time_file, h_time_file)
 
-	desc_folders_path = os.path.join(experiment_folder, asc_folder)
+	desc_folders_path = os.path.join(experiment_folder, desc_folder)
 	q_desc_times, h_desc_times = get_q_h_data(desc_folders_path, q_time_file, h_time_file)
 
-	plt.plot(q_random_times, c='b', title='Quicksort with random array')
-	plt.plot(q_asc_times, c='b', title='Quicksort with asc array')
-	plt.plot(q_asc_times, c='b', title='Quicksort with desc array')
+	csv_output(csv_file_format, random_folder, q_random_times, h_random_times)
+	csv_output(csv_file_format, asc_folder, q_asc_times, h_asc_times)
+	csv_output(csv_file_format, desc_folder, q_desc_times, h_desc_times)
+	# plt.plot(q_random_times, c='b', title='Quicksort with random array')
+	# plt.plot(q_asc_times, c='b', title='Quicksort with asc array')
+	# plt.plot(q_asc_times, c='b', title='Quicksort with desc array')
 
-	plt.plot(h_random_times, c='g', title='Heapsort with random array')
-	plt.plot(h_asc_times, c='b', title='Heapsort with asc array')
-	plt.plot(h_asc_times, c='b', title='Heapsort with desc array')
+	# plt.plot(h_random_times, c='g', title='Heapsort with random array')
+	# plt.plot(h_asc_times, c='b', title='Heapsort with asc array')
+	# plt.plot(h_asc_times, c='b', title='Heapsort with desc array')
 
-	plt.show()
+	# plt.legend()
+	# plt.show()
